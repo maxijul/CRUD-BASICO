187b8b8075e9cfe5b86a903b0e6523a5930ad32c
maxijul <maxijul_14@hotmail.com>
2022-10-13 17:07:28 -0300

    App terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud-practica';
+
+  employeeArray: Employee[] = [
+    {id: 1, name: "Jorge", country: "Argentina"},
+    {id: 2, name: "Angelica", country: "USA"},
+    {id: 3, name: "Victor", country: "England"}
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit(): void{
+
+    if(this.selectedEmployee.id === 0){
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+
+
+    this.selectedEmployee = new Employee();
+  }
+
+  openForEdit(employee: Employee): void{
+    this.selectedEmployee = employee
+  }
+
+  delete(){
+    if (window.confirm("Are you sure you want to delete it?")){
+      this.employeeArray = this.employeeArray.filter(element => element !== this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+  }
+
 }
